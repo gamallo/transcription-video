d7bf3c63d0a93b147deec0b2aea251809e330284
gamallo <pablo.gamallo@gmail.com>
2023-11-26 19:31:38 +0100

    bug fixed

--- transcript_youtube.sh
+++ transcript_youtube.sh
@@ -5,8 +5,6 @@ link=$1
 YOUTUBE=$(echo "$link" | sed -E 's/(https:\/\/www\.youtube\.com\/watch\?v=).*/\1/')
 NAME=$(echo "$link" | sed -E 's/https:\/\/www\.youtube\.com\/watch\?v=(.*)/\1/')
 
-#YOUTUBE="https://www.youtube.com/watch?v="
-#NAME="AJhkLwMvgrg"
 language=$2
 
 FILE=$YOUTUBE""$NAME
@@ -18,6 +16,6 @@ ffmpeg -i $NAME".mp3" -vn -ab 320k -ar 44100 -y $NAME"_edit.mp3"
 
 yt-dlp -S res,ext:mp4:m4a --recode mp4  $FILE -o $NAME.mp4
 
-#python3 transcript.py $NAME $language
+python3 transcript.py $NAME $language
 
 ffmpeg -i $NAME".mp4" -vf subtitles=output-medium/$NAME"_edit.srt" $NAME"_srt.mp4"
